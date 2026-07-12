daeadb855c6aa4ed8d738c1d83a49ef8b5efe43c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,26 @@
-output "spring_cloud_app_mysql_associations" {
-  description = "All spring_cloud_app_mysql_association resources"
-  value       = azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations
-  sensitive   = true
-}
 output "spring_cloud_app_mysql_associations_database_name" {
-  description = "List of database_name values across all spring_cloud_app_mysql_associations"
-  value       = [for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : v.database_name]
+  description = "Map of database_name values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.database_name }
 }
 output "spring_cloud_app_mysql_associations_mysql_server_id" {
-  description = "List of mysql_server_id values across all spring_cloud_app_mysql_associations"
-  value       = [for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : v.mysql_server_id]
+  description = "Map of mysql_server_id values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.mysql_server_id }
 }
 output "spring_cloud_app_mysql_associations_name" {
-  description = "List of name values across all spring_cloud_app_mysql_associations"
-  value       = [for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : v.name]
+  description = "Map of name values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.name }
 }
 output "spring_cloud_app_mysql_associations_password" {
-  description = "List of password values across all spring_cloud_app_mysql_associations"
-  value       = [for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : v.password]
+  description = "Map of password values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.password }
   sensitive   = true
 }
 output "spring_cloud_app_mysql_associations_spring_cloud_app_id" {
-  description = "List of spring_cloud_app_id values across all spring_cloud_app_mysql_associations"
-  value       = [for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : v.spring_cloud_app_id]
+  description = "Map of spring_cloud_app_id values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.spring_cloud_app_id }
 }
 output "spring_cloud_app_mysql_associations_username" {
-  description = "List of username values across all spring_cloud_app_mysql_associations"
-  value       = [for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : v.username]
+  description = "Map of username values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.username }
 }
 
