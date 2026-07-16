02be6bf5737386a809e3c017f259294956b4237c
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 output "spring_cloud_app_mysql_associations_id" {
   description = "Map of id values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
-  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.id }
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "spring_cloud_app_mysql_associations_database_name" {
   description = "Map of database_name values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
-  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.database_name }
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.database_name if v.database_name != null && length(v.database_name) > 0 }
 }
 output "spring_cloud_app_mysql_associations_mysql_server_id" {
   description = "Map of mysql_server_id values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
-  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.mysql_server_id }
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.mysql_server_id if v.mysql_server_id != null && length(v.mysql_server_id) > 0 }
 }
 output "spring_cloud_app_mysql_associations_name" {
   description = "Map of name values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
-  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.name }
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "spring_cloud_app_mysql_associations_password" {
   description = "Map of password values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
-  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.password }
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.password if v.password != null && length(v.password) > 0 }
   sensitive   = true
 }
 output "spring_cloud_app_mysql_associations_spring_cloud_app_id" {
   description = "Map of spring_cloud_app_id values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
-  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.spring_cloud_app_id }
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.spring_cloud_app_id if v.spring_cloud_app_id != null && length(v.spring_cloud_app_id) > 0 }
 }
 output "spring_cloud_app_mysql_associations_username" {
   description = "Map of username values across all spring_cloud_app_mysql_associations, keyed the same as var.spring_cloud_app_mysql_associations"
-  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.username }
+  value       = { for k, v in azurerm_spring_cloud_app_mysql_association.spring_cloud_app_mysql_associations : k => v.username if v.username != null && length(v.username) > 0 }
 }
 
